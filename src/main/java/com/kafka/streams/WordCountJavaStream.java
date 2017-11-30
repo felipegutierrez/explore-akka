afad2bebd425416fc2bff59343441c27015e339b
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2017-11-30 22:46:27 +0100

    Imported project to IntelliJ IDE

--- src/main/java/com/kafka/streams/WordCountJavaStream.java
+++ src/main/java/com/kafka/streams/WordCountJavaStream.java
@@ -1,42 +1,76 @@
 package com.kafka.streams;
 
-import java.util.Arrays;
-import java.util.Properties;
-
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.kstream.ValueMapper;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class WordCountJavaStream {
 
 	public static void main(final String[] args) throws Exception {
-		Properties config = new Properties();
-		config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
-		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-		config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+		Properties props = new Properties();
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+		// setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+		// Note: To re-run the demo, you need to use the offset reset tool:
+		// https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
 		StreamsBuilder builder = new StreamsBuilder();
-		KStream<String, String> textLines = builder.stream("streams-plaintext-input");
-		KTable<String, Long> wordCounts = textLines
-				.flatMapValues(textLine -> Arrays.asList(textLine.toLowerCase().split("\\W+")))
-				.groupBy((key, word) -> word)
-				.count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("counts-store"));
-		wordCounts.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
 
-		KafkaStreams streams = new KafkaStreams(builder.build(), config);
+		KStream<String, String> source = builder.stream("streams-plaintext-input");
 
-		streams.start();
+		KTable<String, Long> counts = source
+				.flatMapValues(new ValueMapper<String, Iterable<String>>() {
+					@Override
+					public Iterable<String> apply(String value) {
+						return Arrays.asList(value.toLowerCase(Locale.getDefault()).split(" "));
+					}
+				})
+				.groupBy(new KeyValueMapper<String, String, String>() {
+					@Override
+					public String apply(String key, String value) {
+						return value;
+					}
+				})
+				.count();
 
-		Thread.sleep(50000L);
+		// need to override value serde to Long type
+		counts.toStream().to("streams-wordcount-output", Produced.with(Serdes.String(), Serdes.Long()));
 
-		streams.close();
+		final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		// attach shutdown handler to catch control-c
+		Runtime.getRuntime().addShutdownHook(new Thread("streams-wordcount-shutdown-hook") {
+			@Override
+			public void run() {
+				streams.close();
+				latch.countDown();
+			}
+		});
+
+		try {
+			streams.start();
+			latch.await();
+		} catch (Throwable e) {
+			System.exit(1);
+		}
+		System.exit(0);
 	}
 }
