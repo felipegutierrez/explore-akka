a2ddb839d679c3f98e3a6deae898426367c00d44
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-10-15 17:13:42 +0200

    :construction: advanced recap of Scala multi threading

--- src/main/scala/org/github/explore/recap/MultiThreadRecap.scala
+++ src/main/scala/org/github/explore/recap/MultiThreadRecap.scala
@@ -33,61 +33,41 @@ object MultiThreadRecap extends App {
   class BankAccount(private var amount: Int) {
     override def toString: String = "" + amount
 
-    def getAmount(): Int = {
-      amount
-    }
+    def getAmount(): Int = amount
 
-    def allowWithdraw(money: Int): Boolean = {
-      if (money <= amount) true
-      else false
-    }
+    //    def allowWithdraw(money: Int): Boolean = {
+    //      if (money <= amount) true
+    //      else false
+    //    }
 
-    def withdraw(money: Int) = {
-      if (allowWithdraw(money)) {
-        this.amount -= money
-      }
-    }
+    def withdraw(money: Int) = this.amount -= money
+
+    def deposit(money: Int) = this.amount += money
 
     def safeWithdraw(money: Int) = this.synchronized {
-      if (allowWithdraw(money)) {
-        this.amount -= money
-      }
+      this.amount -= money
+    }
+
+    def safeDeposit(money: Int) = this.synchronized {
+      this.amount += money
     }
   }
 
   val ba: BankAccount = new BankAccount(50000)
-  val aThirdThread = new Thread(() => {
-    (1 to 20000).foreach(i => {
-      ba.withdraw(i * 100)
-    })
-  })
-  val aFourthThread = new Thread(() => {
-    (1 to 20000).foreach(i => {
-      ba.withdraw(i * 100)
-    })
-  })
-  aThirdThread.start()
-  aFourthThread.start()
-  aThirdThread.join()
-  aFourthThread.join()
+  for (_ <- 1 to 50000) {
+    new Thread(() => ba.withdraw(1)).start()
+  }
+  for (_ <- 1 to 50000) {
+    new Thread(() => ba.deposit(1)).start()
+  }
   println(s"final amount " + ba.getAmount())
 
-  val ba01: BankAccount = new BankAccount(50000)
-  val aFifthThread = new Thread(() => {
-    (1 to 20000).foreach(i => {
-      ba01.safeWithdraw(i * 100)
-    })
-  })
-  val aSixthThread = new Thread(() => {
-    (1 to 20000).foreach(i => {
-      ba01.safeWithdraw(i * 100)
-    })
-  })
-  aFifthThread.start()
-  aSixthThread.start()
-  aFifthThread.join()
-  aSixthThread.join()
-  println(s"safeWithdraw final amount " + ba01.getAmount())
-
-
+  val safeBa: BankAccount = new BankAccount(50000)
+  for (_ <- 1 to 50000) {
+    new Thread(() => safeBa.safeWithdraw(1)).start()
+  }
+  for (_ <- 1 to 50000) {
+    new Thread(() => safeBa.safeDeposit(1)).start()
+  }
+  println(s"safe final amount " + safeBa.getAmount())
 }
