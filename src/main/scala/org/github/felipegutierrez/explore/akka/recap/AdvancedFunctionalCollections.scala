2320021a639dd8815607d75739d734d329de5879
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-10-27 15:59:01 +0100

    :construction: create a Set collection based on functional programming

--- src/main/scala/org/github/felipegutierrez/explore/akka/recap/AdvancedFunctionalCollections.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/recap/AdvancedFunctionalCollections.scala
@@ -35,22 +35,57 @@ object AdvancedFunctionalCollections extends App {
     def size(): Int
     def contains(elem: A): Boolean
     def +(elem: A): MySet[A]
-    def ++(anotherSet: MySet[A]): MySet[A]
+    def ++(anotherSet: MySet[A]): MySet[A] // union
     def map[B](f: A => B): MySet[B]
-    def flatMap[B](f: A => B): MySet[B]
+    def flatMap[B](f: A => MySet[B]): MySet[B]
     def filter(predicate: A => Boolean): MySet[A]
     def foreach(f: A => Unit): Unit
+    def -(elem: A): MySet[A]
+    def --(anotherSet: MySet[A]): MySet[A] // difference
+    def &(anotherSet: MySet[A]): MySet[A] // intersection
+    def unary_! : MySet[A]
   }
 
   class EmptySet[A] extends MySet[A] {
     override def contains(elem: A): Boolean = false
-    def size(): Int = 0
+    override def size(): Int = 0
     override def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
     override def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
     override def map[B](f: A => B): MySet[B] = new EmptySet[B]
-    override def flatMap[B](f: A => B): MySet[B] = new EmptySet[B]
+    override def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
     override def filter(predicate: A => Boolean): MySet[A] = this
     override def foreach(f: A => Unit): Unit = ()
+    override def -(elem: A): MySet[A] = this
+    override def --(anotherSet: MySet[A]): MySet[A] = this
+    override def &(anotherSet: MySet[A]): MySet[A] = this
+    override def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
+  }
+
+  // all elements of type A which satisfy a property
+  // { x in A | property(x) }
+  class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+    override def size(): Int = Int.MaxValue
+    def contains(elem: A): Boolean = property(elem)
+    // { x in A | property(x) } + element = { x in A | property(x) || x == element }
+    def +(elem: A): MySet[A] =
+      new PropertyBasedSet[A](x => property(x) || x == elem)
+
+    // { x in A | property(x) } ++ set => { x in A | property(x) || set contains x }
+    def ++(anotherSet: MySet[A]): MySet[A] =
+      new PropertyBasedSet[A](x => property(x) || anotherSet(x))
+
+    // all integers => (_ % 3) => [0 1 2]
+    def map[B](f: A => B): MySet[B] = politelyFail
+    def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+    def foreach(f: A => Unit): Unit = politelyFail
+
+    def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+    def -(elem: A): MySet[A] = filter(x => x != elem)
+    def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+    def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+    def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+    def politelyFail = throw new IllegalArgumentException("Really deep rabbit hole!")
   }
 
   class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -65,7 +100,7 @@ object AdvancedFunctionalCollections extends App {
       else new NonEmptySet[A](elem, this)
     override def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head
     override def map[B](f: A => B): MySet[B] = tail.map(f) + f(head)
-    override def flatMap[B](f: A => B): MySet[B] = (tail.flatMap(f)) + f(head)
+    override def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
     override def filter(predicate: A => Boolean): MySet[A] = {
       val filteredTail = tail.filter(predicate)
       if (predicate(head)) filteredTail + head
@@ -75,6 +110,13 @@ object AdvancedFunctionalCollections extends App {
       f(head)
       tail.foreach(f)
     }
+    override def -(elem: A): MySet[A] = {
+      if (head == elem) tail
+      else tail.-(elem).+(head)
+    }
+    override def --(anotherSet: MySet[A]): MySet[A] = filter(x => !anotherSet.contains(x))
+    override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet) // intersection is equal to filtering
+    override def unary_! : MySet[A] = this
   }
 }
 
