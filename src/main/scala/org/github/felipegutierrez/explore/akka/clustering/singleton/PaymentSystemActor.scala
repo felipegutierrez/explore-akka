8cffffbafe4029fbab32837f94861f12293f2c71
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-11-20 13:44:10 +0100

    :construction: singleton master with High Availability of 3 replicas on Akka cluster

---
+++ src/main/scala/org/github/felipegutierrez/explore/akka/clustering/singleton/PaymentSystemActor.scala
@@ -0,0 +1,14 @@
+package org.github.felipegutierrez.explore.akka.clustering.singleton
+
+import akka.actor.{Actor, ActorLogging}
+
+case class Order(items: List[String], total: Double)
+
+case class Transaction(orderId: Int, txnId: String, amount: Double)
+
+class PaymentSystemActor extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case t: Transaction => log.info(s"validating transaction $t")
+    case m => log.info(s"received unknown message $m")
+  }
+}
---
+++ src/main/scala/org/github/felipegutierrez/explore/akka/clustering/singleton/PaymentSystemClient.scala
@@ -0,0 +1,38 @@
+package org.github.felipegutierrez.explore.akka.clustering.singleton
+
+import akka.actor.{ActorSystem, Cancellable}
+import akka.cluster.singleton.{ClusterSingletonProxy, ClusterSingletonProxySettings}
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.duration._
+import scala.util.Random
+
+object PaymentSystemClient extends App {
+  val config = ConfigFactory.parseString(
+    """
+      |akka.remote.artery.canonical.port = 0
+    """.stripMargin)
+    .withFallback(ConfigFactory.load("clustering/clusteringSingleton.conf"))
+  val system = ActorSystem("RTJVMCluster", config)
+
+  val proxy = system.actorOf(
+    ClusterSingletonProxy.props(
+      singletonManagerPath = "/user/paymentMasterActor",
+      settings = ClusterSingletonProxySettings(system)
+    ),
+    "paymentSystemProxy"
+  )
+
+  val onlineShopCheckout = system.actorOf(OnlineShopCheckout.props(proxy))
+
+  import system.dispatcher
+
+  val routine: Cancellable = system.scheduler.scheduleWithFixedDelay(5 second, 2 seconds) {
+    new Runnable {
+      def run(): Unit = {
+        val randomOrder = Order(List(), Random.nextDouble() * 100)
+        onlineShopCheckout ! randomOrder
+      }
+    }
+  }
+}
