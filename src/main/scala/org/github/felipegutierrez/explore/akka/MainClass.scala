e196f64003a9ea9c0fb2b3524142eae2cee58f0f
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-11-24 16:26:58 +0100

    :construction: recovering example using Akka persistence

--- src/main/scala/org/github/felipegutierrez/explore/akka/MainClass.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/MainClass.scala
@@ -9,7 +9,7 @@ import org.github.felipegutierrez.explore.akka.clustering.wordcount.{ClusteringW
 import org.github.felipegutierrez.explore.akka.falttolerance._
 import org.github.felipegutierrez.explore.akka.infra._
 import org.github.felipegutierrez.explore.akka.patterns._
-import org.github.felipegutierrez.explore.akka.persistence.event_sourcing.{PersistentActors, PersistentActorsExercise, Snapshots}
+import org.github.felipegutierrez.explore.akka.persistence.event_sourcing.{PersistentActors, PersistentActorsExercise, RecoveryDemo, Snapshots}
 import org.github.felipegutierrez.explore.akka.recap._
 import org.github.felipegutierrez.explore.akka.remote._
 import org.github.felipegutierrez.explore.akka.remote.deployment.{LocalDeployment, RemoteDeployment}
@@ -84,7 +84,7 @@ object MainClass extends App {
   println(s"46 - PersistentActors")
   println(s"47 - PersistentActorsExercise")
   println(s"48 - Snapshots")
-  println(s"49 - ")
+  println(s"49 - RecoveryDemo")
   println(s"50 - ")
 
   var option: String = ""
@@ -188,7 +188,7 @@ object MainClass extends App {
     case "46" => PersistentActors.run()
     case "47" => PersistentActorsExercise.run()
     case "48" => Snapshots.run()
-    case "49" => ???
+    case "49" => RecoveryDemo.run()
     case "50" => ???
     case _ => println("option unavailable.")
   }
