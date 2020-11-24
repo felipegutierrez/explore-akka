96e69e7ded72c56d3f36e17dd1b697f17b700bbb
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-11-24 13:49:40 +0100

    :rocket: add persistent feature to the Akka cluster singleton voting system

--- src/main/scala/org/github/felipegutierrez/explore/akka/clustering/voting/VotingAggregator.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/clustering/voting/VotingAggregator.scala
@@ -1,6 +1,7 @@
 package org.github.felipegutierrez.explore.akka.clustering.voting
 
 import akka.actor.{Actor, ActorLogging, ReceiveTimeout}
+import akka.persistence.PersistentActor
 
 import scala.concurrent.duration._
 
@@ -14,16 +15,53 @@ object VoteMessages {
 
 }
 
-class VotingAggregator extends Actor with ActorLogging {
+class VotingAggregator extends PersistentActor with ActorLogging {
 
   import VoteMessages._
 
   val CANDIDATES: Set[String] = Set("Martin", "Roland", "Jonas", "Daniel")
-
+  var personsVoted: Set[String] = Set()
+  var polls: Map[String, Int] = Map()
   context.setReceiveTimeout(60 seconds)
 
-  override def receive: Receive = online(Set(), Map())
+  override def persistenceId: String = "voting-persistent-agg"
 
+  override def receiveCommand: Receive = {
+    case v@Vote(Person(id, age), candidate) =>
+      log.info(s"received vote $v")
+      if (personsVoted.contains(id)) sender() ! VoteRejected("already voted")
+      else if (age < 18) sender() ! VoteRejected("not above legal voting age")
+      else if (!CANDIDATES.contains(candidate)) sender() ! VoteRejected("invalid candidate")
+      else {
+        /* When we receive a command
+         * 1 - we create an EVENT to persist into the store
+         * 2 - we persist the event, pass a callback that will get triggered once the event is written
+         * 3 - we update the actor's state when the event has persisted
+         */
+        persist(v) { e =>
+          personsVoted += v.person.id
+          val candidateVotes = polls.getOrElse(candidate, 0)
+          polls += (candidate -> (candidateVotes + 1))
+          sender() ! VoteAccepted
+          log.info(s"Recording vote from person $id for $candidate")
+        }
+      }
+    case ReceiveTimeout =>
+      log.info(s"TIME'S UP, here are the poll results: $polls")
+      context.setReceiveTimeout(Duration.Undefined)
+      context.become(offline)
+  }
+
+  override def receiveRecover: Receive = {
+    /** Best practice: follow the logic in the persist step of receiveCommand */
+    case v@Vote(Person(id, age), candidate) =>
+      personsVoted += v.person.id
+      val candidateVotes = polls.getOrElse(candidate, 0)
+      polls += (candidate -> (candidateVotes + 1))
+      log.info(s"recovered vote from ${v.person} to $candidate ,polls: $polls")
+  }
+
+  // override def receive: Receive = online(Set(), Map())
   /**
    * TODO: enhance personsVoted and polls with Akka persistence.
    * Since this actor handles state in personsVoted and polls we have to persist
