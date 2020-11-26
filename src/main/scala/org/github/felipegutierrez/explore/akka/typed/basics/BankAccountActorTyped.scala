c4dc28b8a971afb9ff1dbc5dc312c6ccee5e0235
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-11-26 17:00:45 +0100

    :tada: Bank account with Akka typed and stateless

--- src/main/scala/org/github/felipegutierrez/explore/akka/typed/basics/BankAccountActorTyped.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/typed/basics/BankAccountActorTyped.scala
@@ -5,69 +5,71 @@ import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 
 object BankAccountActorTyped {
 
+  import BankAccountDomain._
+  import PersonDomain._
+
+  val person: Behavior[PersonTxn] = Behaviors.setup { context =>
+    Behaviors.receiveMessage {
+      case LiveTheLife(account) =>
+        context.log.info(s"received live the life with account: $account")
+        account ! Balance(context.self)
+        account ! Deposit(1000, context.self)
+        account ! Deposit(2000, context.self)
+        account ! Deposit(500, context.self)
+        account ! Balance(context.self)
+        account ! Withdraw(500, context.self)
+        account ! Deposit(5000, context.self)
+        account ! Balance(context.self)
+        account ! Withdraw(2000, context.self)
+        account ! Balance(context.self)
+        Behaviors.same
+      case msg =>
+        context.log.info(s"received msg: $msg")
+        Behaviors.same
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     run()
   }
-
+  
   def run(): Unit = {
-    import BankAccountDomain._
     import PersonDomain._
-    
-    val bankAccount: Behavior[BankAccountTxn] = Behaviors.setup { context =>
-      var totalBalance = 0
-      Behaviors.receiveMessage {
-        case deposit@Deposit(value, from) =>
-          if (deposit.value <= 0) {
-            deposit.from ! TransactionFailure(s"deposit value not allowed: ${deposit.value}")
-          } else {
-            totalBalance += deposit.value
-            deposit.from ! TransactionSuccess(s"success deposit of ${deposit.value}")
-          }
-          Behaviors.same
-        case withdraw@Withdraw(value, from) =>
-          if (withdraw.value <= 0) {
-            withdraw.from ! TransactionFailure(s"withdraw value not allowed: ${withdraw.value}")
-          } else if (withdraw.value > totalBalance) {
-            withdraw.from ! TransactionFailure(s"insufficient total balance: ")
-          } else {
-            totalBalance -= withdraw.value
-            withdraw.from ! TransactionSuccess(s"success withdraw of: ${withdraw.value}")
-          }
-          Behaviors.same
-        case balance@Balance(from) =>
-          balance.from ! TransactionSuccess(s"Balance: $totalBalance")
-          Behaviors.same
-      }
-    }
 
-    val person: Behavior[PersonTxn] = Behaviors.setup { context =>
-      Behaviors.receiveMessage {
-        case LiveTheLife(account) =>
-          context.log.info(s"received live the life with account: $account")
-          account ! Balance(context.self)
-          account ! Deposit(1000, context.self)
-          account ! Deposit(2000, context.self)
-          account ! Deposit(500, context.self)
-          account ! Balance(context.self)
-          account ! Withdraw(500, context.self)
-          account ! Deposit(5000, context.self)
-          account ! Balance(context.self)
-          account ! Withdraw(2000, context.self)
-          account ! Balance(context.self)
-          Behaviors.same
-        case msg =>
-          context.log.info(s"received msg: $msg")
-          Behaviors.same
-      }
-    }
-
-    val bankAccountActor = ActorSystem(bankAccount, "BankAccountSystem")
+    val bankAccountActor = ActorSystem(bankAccount(0), "BankAccountSystem")
     val actorPerson = ActorSystem(person, "BankAccountSystem")
     actorPerson ! LiveTheLife(bankAccountActor)
 
     Thread.sleep(5000)
     actorPerson.terminate()
     bankAccountActor.terminate()
+  }
+
+  def bankAccount(totalBalance: Int): Behavior[BankAccountTxn] = Behaviors.setup { context =>
+    Behaviors.receiveMessage {
+      case deposit@Deposit(value, from) =>
+        if (deposit.value <= 0) {
+          deposit.from ! TransactionFailure(s"deposit value not allowed: ${deposit.value}")
+          bankAccount(totalBalance)
+        } else {
+          deposit.from ! TransactionSuccess(s"success deposit of ${deposit.value}")
+          bankAccount(totalBalance + deposit.value)
+        }
+      case withdraw@Withdraw(value, from) =>
+        if (withdraw.value <= 0) {
+          withdraw.from ! TransactionFailure(s"withdraw value not allowed: ${withdraw.value}")
+          bankAccount(totalBalance)
+        } else if (withdraw.value > totalBalance) {
+          withdraw.from ! TransactionFailure(s"insufficient total balance: ")
+          bankAccount(totalBalance)
+        } else {
+          withdraw.from ! TransactionSuccess(s"success withdraw of: ${withdraw.value}")
+          bankAccount(totalBalance - withdraw.value)
+        }
+      case balance@Balance(from) =>
+        balance.from ! TransactionSuccess(s"Balance: $totalBalance")
+        bankAccount(totalBalance)
+    }
   }
 
   object BankAccountDomain {
