9dc563ef1d2bb7d9115e4aa6a8305cf0266839d3
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-03 13:20:56 +0100

    :construction: graph Akka stream exposing a materializer using high order function and Future

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphMaterializedValues.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphMaterializedValues.scala
@@ -2,7 +2,10 @@ package org.github.felipegutierrez.explore.akka.classic.streams.graphs
 
 import akka.actor.ActorSystem
 import akka.stream.SinkShape
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Sink, Source}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Sink, Source}
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object StreamOpenGraphMaterializedValues extends App {
 
@@ -17,20 +20,29 @@ object StreamOpenGraphMaterializedValues extends App {
 
     // Step 1
     val complexWordSink = Sink.fromGraph(
-      GraphDSL.create() { implicit builder =>
-        import GraphDSL.Implicits._
-        // step 2 - shapes
-        val broadcast = builder.add(Broadcast[String](2))
-        val lowercaseFlow = builder.add(Flow[String].filter(word => word == word.toLowerCase))
-        val shortStringFilter = builder.add(Flow[String].filter(_.length < 5))
+      GraphDSL.create(counter) { implicit builder =>
+        counterShape =>
+          import GraphDSL.Implicits._
+          // step 2 - shapes
+          val broadcast = builder.add(Broadcast[String](2))
+          val lowercaseFlow = builder.add(Flow[String].filter(word => word == word.toLowerCase))
+          val shortStringFilter = builder.add(Flow[String].filter(_.length < 3))
 
-        // step 3 - tie components together
-        broadcast ~> lowercaseFlow ~> printer
-        broadcast ~> shortStringFilter ~> counter
+          // step 3 - tie components together
+          broadcast ~> lowercaseFlow ~> printer
+          broadcast ~> shortStringFilter ~> counterShape
 
-        // Step 4 - the Shape
-        SinkShape(broadcast.in)
+          // Step 4 - the Shape
+          SinkShape(broadcast.in)
       }
     )
+
+    import system.dispatcher
+    val shortStringsCounterFuture: Future[Int] = wordSource.toMat(complexWordSink)(Keep.right).run()
+    shortStringsCounterFuture.onComplete {
+      case Success(value) => println(s"total of words: $value")
+      case Failure(exception) => println(s"fail because: $exception")
+    }
+
   }
 }
