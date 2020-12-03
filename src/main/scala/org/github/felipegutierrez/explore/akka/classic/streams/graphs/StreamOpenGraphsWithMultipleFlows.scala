7357c93bf2d1bb16f0b5e616dec232d1c33d3408
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-03 17:54:18 +0100

    :tada: graph Akka stream using partition to construct dynamic Flows

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphsWithMultipleFlows.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphsWithMultipleFlows.scala
@@ -2,7 +2,7 @@ package org.github.felipegutierrez.explore.akka.classic.streams.graphs
 
 import akka.actor.ActorSystem
 import akka.stream.FlowShape
-import akka.stream.scaladsl.{Flow, GraphDSL, Sink, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition, Sink, Source}
 
 import scala.concurrent.duration._
 
@@ -39,7 +39,8 @@ object StreamOpenGraphsWithMultipleFlows extends App {
       result
     }
 
-    def isMultipleOf(value: Int, multiple: Int): Boolean = (value % multiple) == 0
+    // def isMultipleOf(value: Int, multiple: Int): Int = value % multiple
+    def isMultipleOf(value: Int, multiple: Int): Int = if (value % multiple == 0) 0 else 1
 
     // Step 1 - setting up the fundamental for a stream graph
     val complexFlowIncrementer = Flow.fromGraph(
@@ -48,17 +49,20 @@ object StreamOpenGraphsWithMultipleFlows extends App {
         // Step 2 - add necessary components of this graph
         val incrementerShape = builder.add(incrementer)
         val multiplierShape = builder.add(multiplier)
+        val dividerShape = builder.add(divider)
 
-        // println(s"builder.materializedValue: ${builder.materializedValue}")
+        //add partition and merge if(isMultipleOf(_, 10)) 0 else 1
+        // val partition = builder.add(Partition[Int](2, if(isMultipleOf(_, 10)) 0 else 1))
+        val partition = builder.add(Partition[Int](2, isMultipleOf(_, 10)))
+        val merge = builder.add(Merge[Int](2))
 
         // Step 3 - tying up the components
-        incrementerShape ~> multiplierShape
-        // BUT I WOULD LIKE TO DO SOMETHING AS BELOW
-        // if (isMultipleOf(value???, 10)) incrementerShape ~> divider
-        // else incrementerShape ~> multiplierShape
+        incrementerShape ~> partition
+        partition.out(0) ~> dividerShape ~> merge.in(0)
+        partition.out(1) ~> multiplierShape ~> merge.in(1)
 
         // Step 4 - return the shape
-        FlowShape(incrementerShape.in, multiplierShape.out)
+        FlowShape(incrementerShape.in, merge.out)
       }
     )
     // run the graph and materialize it
