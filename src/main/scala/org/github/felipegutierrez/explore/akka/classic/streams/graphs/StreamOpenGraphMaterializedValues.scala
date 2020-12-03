d54a022bc5862dcde971c77e09466edfe070505e
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-03 13:53:03 +0100

    :construction: graph Akka stream exposing a materializer composed values using high order function and Future

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphMaterializedValues.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphMaterializedValues.scala
@@ -1,8 +1,8 @@
 package org.github.felipegutierrez.explore.akka.classic.streams.graphs
 
 import akka.actor.ActorSystem
-import akka.stream.SinkShape
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Sink, Source}
+import akka.stream.{FlowShape, SinkShape}
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
@@ -44,5 +44,40 @@ object StreamOpenGraphMaterializedValues extends App {
       case Success(value) => println(s"total of words: $value")
       case Failure(exception) => println(s"fail because: $exception")
     }
+
+    val simpleSource = Source(1 to 42)
+    val simpleFlow = Flow[Int].map(x => x)
+    val simpleSink = Sink.ignore
+
+    val enhancedFlowCountFuture = simpleSource
+      .viaMat(enhanceFlow(simpleFlow))(Keep.right)
+      .toMat(simpleSink)(Keep.left)
+      .run()
+    
+    enhancedFlowCountFuture.onComplete {
+      case Success(count) => println(s"$count elements went through the enhanced flow")
+      case _ => println("Something failed")
+    }
+  }
+
+  def enhanceFlow[A, B](flow: Flow[A, B, _]): Flow[A, B, Future[Int]] = {
+
+    val sinkCounter = Sink.fold[Int, B](0)((count, value) => count + 1)
+
+    // Step 1
+    val complexWordSink = Flow.fromGraph(
+      GraphDSL.create(sinkCounter) { implicit builder =>
+        counterSinkShape =>
+          import GraphDSL.Implicits._
+          // step 2 - shapes
+          val broadcast = builder.add(Broadcast[B](2))
+          val originalFlowShape = builder.add(flow)
+          // step 3 - tie components together
+          originalFlowShape ~> broadcast ~> counterSinkShape
+          // step 4 - the Shape
+          FlowShape(originalFlowShape.in, broadcast.out(1))
+      }
+    )
+    complexWordSink
   }
 }
