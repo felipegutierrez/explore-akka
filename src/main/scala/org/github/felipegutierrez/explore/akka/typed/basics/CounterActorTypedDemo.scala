4e2de02ad00d60992dc7f4a0de7d2344504fdbe2
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-04 10:04:14 +0100

    :tada: added logging to Akka typed

--- src/main/scala/org/github/felipegutierrez/explore/akka/typed/basics/CounterActorTypedDemo.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/typed/basics/CounterActorTypedDemo.scala
@@ -2,6 +2,7 @@ package org.github.felipegutierrez.explore.akka.typed.basics
 
 import akka.actor.typed._
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+import com.typesafe.config.ConfigFactory
 
 object CounterActorTypedDemo {
   def main(args: Array[String]): Unit = {
@@ -12,7 +13,8 @@ object CounterActorTypedDemo {
 
     import Counter._
 
-    val countActor = ActorSystem(CounterActorTyped(), "CounterSystem")
+    val specialConfig = ConfigFactory.load().getConfig("mySpecialConfig")
+    val countActor = ActorSystem(CounterActorTyped(), "CounterSystem", specialConfig)
     (1 to 5).foreach(_ => countActor ! Increment)
     (1 to 3).foreach(_ => countActor ! Decrement)
     countActor ! Print
@@ -46,15 +48,15 @@ class CounterActorTyped(context: ActorContext[Counter.CounterMsg]) extends Abstr
 
   override def onMessage(msg: CounterMsg): Behavior[CounterMsg] = msg match {
     case Increment =>
-      context.log.info(s"incrementing $count ...")
+      context.log.debug(s"incrementing $count ...")
       count += 1
       Behaviors.same
     case Decrement =>
-      context.log.info(s"decrementing $count ...")
+      context.log.debug(s"decrementing $count ...")
       count -= 1
       Behaviors.same
     case Print =>
-      context.log.info(s"current count is: $count")
+      context.log.debug(s"current count is: $count")
       Behaviors.same
   }
 
