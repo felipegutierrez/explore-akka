e90436c9548ca194b681a171e408a330c4baf82d
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-04 16:50:30 +0100

    :tada: changed Flow Shape

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphWithTwoSourcesAndDifferentJoinStrategies.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/StreamOpenGraphWithTwoSourcesAndDifferentJoinStrategies.scala
@@ -2,7 +2,7 @@ package org.github.felipegutierrez.explore.akka.classic.streams.graphs
 
 import akka.actor.ActorSystem
 import akka.stream.ClosedShape
-import akka.stream.scaladsl.{Concat, Flow, GraphDSL, Merge, Partition, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Partition, RunnableGraph, Sink, Source, Zip}
 
 import scala.concurrent.duration._
 
@@ -14,23 +14,21 @@ object StreamOpenGraphWithTwoSourcesAndDifferentJoinStrategies extends App {
   def run() = {
     implicit val system = ActorSystem("StreamOpenGraphWithTwoSourcesAndDifferentJoinStrategies")
 
-    val MULTI = 10
-    val DIVIDE = 2
-    val slowSource = Source(1 to 1000).throttle(5, 1 second)
-    val fastSource = Source(1 to 1000).throttle(50, 1 second)
+    val crescentSource = Source(1 to 1000).throttle(10, 1 second)
+    val decrescentSource = Source(1000 to 2000).throttle(10, 1 second)
 
-    val strategy01 = Flow[Int].map { x =>
-      val result = x * MULTI
-      print(s" | strategy01 $x * $MULTI -> $result")
+    val strategy0 = Flow[(Int, Int)].map { pair =>
+      val result = pair._1 + pair._2
+      print(s"strategy 0 [${pair._1} + ${pair._2}] = $result")
       result
     }
-    val strategy02 = Flow[Int].map { x =>
-      val result = x / DIVIDE
-      print(s" | strategy02 $x / $DIVIDE -> $result")
+    val strategy1 = Flow[(Int, Int)].map { pair =>
+      val result = pair._1 - pair._2
+      print(s"strategy 1 [${pair._1} - ${pair._2}] = $result")
       result
     }
 
-    def strategicJoinDecision(value: Int, multiple: Int): Int = if (value % multiple == 0) 0 else 1
+    def strategicJoinDecision(value: (Int, Int)): Int = if (value._1 < value._2) 0 else 1
 
     // Step 1 - setting up the fundamental for a stream graph
     val switchJoinStrategies = RunnableGraph.fromGraph(
@@ -38,17 +36,17 @@ object StreamOpenGraphWithTwoSourcesAndDifferentJoinStrategies extends App {
         import GraphDSL.Implicits._
 
         // Step 2 - add partition and merge strategy
-        val concatShape = builder.add(Concat[Int](2))
-        val partitionDecisionShape = builder.add(Partition[Int](2, strategicJoinDecision(_, 10)))
-        val strategy01Shape = builder.add(strategy01)
-        val strategy02Shape = builder.add(strategy02)
+        val zipShape = builder.add(Zip[Int, Int])
+        val partitionDecisionShape = builder.add(Partition[(Int, Int)](2, strategicJoinDecision(_)))
+        val strategy01Shape = builder.add(strategy0)
+        val strategy02Shape = builder.add(strategy1)
         val mergeStrategyShape = builder.add(Merge[Int](2))
-        val sinkShape = builder.add(Sink.foreach[Int](println))
+        val sinkShape = builder.add(Sink.foreach[Int](x => println(s" > sink: $x")))
 
         // Step 3 - tying up the components
-        slowSource ~> concatShape.in(0)
-        fastSource ~> concatShape.in(1)
-        concatShape.out ~> partitionDecisionShape
+        crescentSource ~> zipShape.in0
+        decrescentSource ~> zipShape.in1
+        zipShape.out ~> partitionDecisionShape
         partitionDecisionShape.out(0) ~> strategy01Shape ~> mergeStrategyShape.in(0)
         partitionDecisionShape.out(1) ~> strategy02Shape ~> mergeStrategyShape.in(1)
         mergeStrategyShape ~> sinkShape
