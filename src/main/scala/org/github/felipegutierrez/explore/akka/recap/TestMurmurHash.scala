31f87fb3d77c2adca2dfd2a66cfa7b2099ce0c29
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-07 14:05:11 +0100

    update equal method MurmurHash

--- src/main/scala/org/github/felipegutierrez/explore/akka/recap/TestMurmurHash.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/recap/TestMurmurHash.scala
@@ -2,17 +2,27 @@ package org.github.felipegutierrez.explore.akka.recap
 
 import scala.util.hashing.MurmurHash3
 
-object TestMurmurHash extends App {
+object TestMurmurHash {
 
-  override def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
     val email = MyObject("my_email@google.com")
     println(s"This is my email hash: $email and this is my hash: ${email.hashCode()}")
 
   }
 
   case class MyObject(val email: String) {
+
+    override def equals(o: Any): Boolean = {
+      this.hashCode() == o.hashCode()
+    }
+
     override def hashCode(): Int = {
       MurmurHash3.stringHash(email)
     }
+
+    def mailToHash(): Int = {
+      MurmurHash3.stringHash(email)
+    }
   }
+
 }
