790856f3cf193d84c2b3dbf4388eda41cf3b688a
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-11 16:31:29 +0100

    :construction: created a generic custom balancer shape with M inputs and N outputs

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/advanced/StreamCustomGraphShapes.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/advanced/StreamCustomGraphShapes.scala
@@ -68,12 +68,50 @@ object StreamCustomGraphShapes {
         ClosedShape
       }
     )
-    balance2x3Graph.run()
+    // balance2x3Graph.run()
 
     /**
      * Exercise: generalize the balance component, make it M x N
      */
+    def balanceMxNImpl(inputPorts: Int, outputPorts: Int) = {
+      GraphDSL.create() { implicit builder =>
+        import GraphDSL.Implicits._
+        val mergeShape = builder.add(Merge[Int](inputPorts))
+        val balanceShape = builder.add(Balance[Int](outputPorts))
+        mergeShape ~> balanceShape
+        BalanceMxN(mergeShape.inlets.toList, balanceShape.outlets.toList)
+      }
+    }
 
+    val balanceMxNGraph = RunnableGraph.fromGraph(
+      GraphDSL.create() { implicit builder =>
+        import GraphDSL.Implicits._
+
+        val slowSource = Source(Stream.from(1000)).throttle(1, 1 second)
+        val fastSource = Source(Stream.from(1)).throttle(10, 1 second)
+
+        def createSink(index: Int) = Sink.fold(0)((count: Int, element: Int) => {
+          println(s"[sink $index] Received $element, current count is $count")
+          count + 1
+        })
+
+        val sink1 = builder.add(createSink(1))
+        val sink2 = builder.add(createSink(2))
+        val sink3 = builder.add(createSink(3))
+
+        val balance2x3 = builder.add(balanceMxNImpl(2, 3))
+
+        slowSource ~> balance2x3.ins(0)
+        fastSource ~> balance2x3.ins(1)
+
+        balance2x3.outs(0) ~> sink1
+        balance2x3.outs(1) ~> sink2
+        balance2x3.outs(2) ~> sink3
+
+        ClosedShape
+      }
+    )
+    balanceMxNGraph.run()
   }
 
   // balance 2x3 shape
@@ -91,6 +129,15 @@ object StreamCustomGraphShapes {
       out1.carbonCopy(),
       out2.carbonCopy()
     )
+  }
+
+  // a generic balance MxN shape
+  case class BalanceMxN(ins: List[Inlet[Int]], outs: List[Outlet[Int]]) extends Shape {
+    // Inlet[T], Outlet[T]
+    override val inlets: immutable.Seq[Inlet[_]] = ins
+    override val outlets: immutable.Seq[Outlet[_]] = outs
+
+    override def deepCopy(): Shape = BalanceMxN(ins.map(_.carbonCopy()), outs.map(_.carbonCopy()))
   }
 
 }
