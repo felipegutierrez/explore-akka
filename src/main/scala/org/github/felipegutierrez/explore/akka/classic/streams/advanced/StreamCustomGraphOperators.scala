c1cc3f00d407659589904991c808fe97e4bcb354
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-15 12:54:41 +0100

    :tada: created a counter operator for akka stream with materialized value

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/advanced/StreamCustomGraphOperators.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/advanced/StreamCustomGraphOperators.scala
@@ -2,12 +2,13 @@ package org.github.felipegutierrez.explore.akka.classic.streams.advanced
 
 import akka.actor.ActorSystem
 import akka.stream._
-import akka.stream.scaladsl.{Flow, Sink, Source}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.stage._
 import com.typesafe.config.ConfigFactory
 
 import scala.collection.mutable
-import scala.util.Random
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Random, Success}
 
 object StreamCustomGraphOperators {
   def main(args: Array[String]): Unit = {
@@ -18,6 +19,7 @@ object StreamCustomGraphOperators {
     val configString =
       """
         | akka {
+        |   allow-java-serialization = on
         |   loglevel = "DEBUG"
         | }
       """.stripMargin
@@ -32,7 +34,20 @@ object StreamCustomGraphOperators {
     randomGeneratorSource
       .via(filterFlow)
       .to(batchSink)
-      .run()
+     .run()
+
+    val counterFlow = Flow.fromGraph(new CounterFlow[Int])
+//    val countFuture = Source(1 to 10)
+//      // .map(x => if (x == 7) throw new RuntimeException("gotcha!") else x)
+//      .viaMat(counterFlow)(Keep.right)
+//      // .to(Sink.foreach(x => if (x == 7) throw new RuntimeException("gotcha, sink!") else println(x)))
+//      .to(Sink.foreach[Int](println))
+//      .run()
+//    import system.dispatcher
+//    countFuture.onComplete {
+//      case Success(count) => println(s"The number of elements passed: $count")
+//      case Failure(ex) => println(s"Counting the elements failed: $ex")
+//    }
   }
 
   // 1 - a custom source which emits random numbers until canceled
@@ -130,6 +145,56 @@ object StreamCustomGraphOperators {
 
     // step 2: construct a new shape
     override def shape: FlowShape[T, T] = FlowShape[T, T](in, out)
+  }
+
+  /**
+   * Graph stage with materialized values
+   */
+  class CounterFlow[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], Future[Int]] {
+
+    override val shape = FlowShape(inPort, outPort)
+    val inPort = Inlet[T]("counterInt")
+    val outPort = Outlet[T]("counterOut")
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Int]) = {
+
+      val promise = Promise[Int]
+      val logic = new GraphStageLogic(shape) {
+        // setting mutable state
+        var counter = 0
+
+        setHandler(outPort, new OutHandler {
+          override def onPull(): Unit = pull(inPort)
+
+          override def onDownstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onDownstreamFinish()
+          }
+        })
+
+        setHandler(inPort, new InHandler {
+          override def onPush(): Unit = {
+            // extract the element
+            val nextElement = grab(inPort)
+            counter += 1
+            // pass it on
+            push(outPort, nextElement)
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onUpstreamFinish()
+          }
+
+          override def onUpstreamFailure(ex: Throwable): Unit = {
+            promise.failure(ex)
+            super.onUpstreamFailure(ex)
+          }
+        })
+      }
+
+      (logic, promise.future)
+    }
   }
 
 }
