4f15852c75841b8032e560d7c7c19f1c2c8c4b33
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-15 18:08:05 +0100

    :tada: created my batch flow operator in Akka Stream

---
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/advanced/StreamBatchFlowGraphOperators.scala
@@ -0,0 +1,80 @@
+package org.github.felipegutierrez.explore.akka.classic.streams.advanced
+
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.stage._
+import com.typesafe.config.ConfigFactory
+
+import scala.collection.mutable
+import scala.concurrent.duration._
+
+object StreamBatchFlowGraphOperators {
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+
+  def run() = {
+    val configString =
+      """
+        | akka {
+        |   allow-java-serialization = on
+        |   loglevel = "DEBUG"
+        | }
+      """.stripMargin
+    val config = ConfigFactory.parseString(configString)
+    implicit val system = ActorSystem("StreamBatchFlowGraphOperators", ConfigFactory.load(config))
+
+    val source = Source(Stream.from(1)).throttle(5, 1 second).log("source")
+    val sink = Sink.foreach(println)
+    val batchFlow = Flow.fromGraph(new BatchFlow[Int](10))
+    source
+      .via(batchFlow)
+      .to(sink)
+      .run()
+  }
+
+  // step 0: define the shape
+  class BatchFlow[T](maxBatchSize: Int) extends GraphStage[FlowShape[T, T]] {
+    // step 1: define the ports and the component-specific members
+    val in = Inlet[T]("BatchFlow.in")
+    val out = Outlet[T]("BatchFlow.out")
+
+    // step 3: create the logic
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      // mutable state
+      val batch = new mutable.Queue[T]
+
+      // step 4: define mutable state implement my logic here
+      setHandler(in, new InHandler {
+        override def onPush(): Unit = {
+          try {
+            val nextElement = grab(in)
+            batch.enqueue(nextElement)
+            // Thread.sleep(50) // simulate an expensive computation
+            if (batch.size >= maxBatchSize) {
+              // forward the element to the downstream operator
+              batch.dequeueAll(_ => true).foreach { value =>
+                println(value)
+                // push(out, value)
+              }
+            }
+            // send demand upstream signal, asking for another element
+            pull(in)
+          } catch {
+            case e: Throwable => failStage(e)
+          }
+        }
+      })
+      setHandler(out, new OutHandler {
+        override def onPull(): Unit = {
+          pull(in)
+        }
+      })
+    }
+
+    // step 2: construct a new shape
+    override def shape: FlowShape[T, T] = FlowShape[T, T](in, out)
+  }
+
+}
