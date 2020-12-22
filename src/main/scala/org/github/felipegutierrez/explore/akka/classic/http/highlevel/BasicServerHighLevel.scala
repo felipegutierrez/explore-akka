89447aa3721302c68255a64a6e243576c2949e35
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-22 15:07:37 +0100

    :construction: Akka HTTP high level API GET and POST with chain operator

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/BasicServerHighLevel.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/BasicServerHighLevel.scala
@@ -16,12 +16,30 @@ object BasicServerHighLevel {
     // import system.dispatcher
 
     val simpleRoute: Route =
-      path("home") { // Directive
+      path("status") { // Directive
         get {
           complete(StatusCodes.OK) // Directive
+        } ~ post {
+          complete(StatusCodes.Forbidden)
+        }
+      } ~ path("home") {
+        get {
+          complete(HttpEntity(
+            ContentTypes.`text/html(UTF-8)`,
+            """
+              |<html>
+              | <body>hello from Akka HTTP high level API</body>
+              |</html>
+              |""".stripMargin
+          ))
+        } ~ post {
+          complete(StatusCodes.Forbidden)
         }
       }
 
+    println("try:")
+    println("http GET localhost:8080/status")
+    println("http POST localhost:8080/status")
     println("http GET localhost:8080/home")
     println("http POST localhost:8080/home")
     Http().newServerAt("localhost", 8080).bindFlow(simpleRoute)
