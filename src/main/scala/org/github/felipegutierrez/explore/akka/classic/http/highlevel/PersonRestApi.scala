c9356b035a890fa54fb6d9e120003a309d5862f6
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-23 11:52:25 +0100

    :construction: Person API using high level Akka Http

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/PersonRestApi.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/PersonRestApi.scala
@@ -1,6 +1,16 @@
 package org.github.felipegutierrez.explore.akka.classic.http.highlevel
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest}
+import akka.http.scaladsl.server.Directives.{parameter, _}
+import akka.pattern.ask
+import akka.util.Timeout
+import org.github.felipegutierrez.explore.akka.classic.http.highlevel.PersonDomain.CreatePerson
+import spray.json.{DefaultJsonProtocol, _}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
 
 /**
  * Exercise:
@@ -13,21 +23,104 @@ import akka.actor.ActorSystem
  *     - extract the request
  *     - process the entity's data
  */
-object PersonRestApi {
+
+case class Person(pin: Int, name: String)
+
+trait PersonJsonProtocol extends DefaultJsonProtocol {
+  implicit val personFormat = jsonFormat2(Person)
+}
+
+object PersonDomain {
+
+  case class CreatePerson(person: Person)
+
+  case class PersonCreated(pin: Int)
+
+  case class FindPerson(pin: Int)
+
+  case object FindAllPeople
+
+}
+
+class PersonActor extends Actor with ActorLogging {
+
+  import PersonDomain._
+
+  var people: Map[Int, Person] = Map()
+
+  override def receive: Receive = {
+    case CreatePerson(person) =>
+      log.info(s"adding person $person")
+      people = people + (person.pin -> person)
+      sender() ! PersonCreated(person.pin)
+    case FindPerson(pin) =>
+      log.info(s"searching for person $pin")
+      sender() ! people.get(pin)
+    case FindAllPeople =>
+      log.info(s"searching for all people")
+      sender() ! people.values.toList
+  }
+}
+
+object PersonRestApi extends PersonJsonProtocol {
   def main(args: Array[String]): Unit = {
     run()
   }
+
   def run() = {
     implicit val system = ActorSystem("PersonRestApi")
     import system.dispatcher
 
-    var people = List(
-      Person(1, "Alice"),
-      Person(2, "Bob"),
-      Person(3, "Charlie")
-    )
+    val personActor = system.actorOf(Props[PersonActor], "PersonActor")
+    List(Person(1, "Alice"), Person(2, "Bob"), Person(3, "Charlie"))
+      .foreach { p =>
+        personActor ! CreatePerson(p)
+      }
 
+    def toHttpEntity(payload: String) = HttpEntity(ContentTypes.`application/json`, payload)
+
+    implicit val defaultTimeout = Timeout(2 seconds)
+    import PersonDomain._
+    val personRoutes =
+      (pathPrefix("api" / "people") & get) {
+        (path(IntNumber) | parameter('pin.as[Int])) { pin: Int =>
+          println(s"retrieve the person with that PIN $pin")
+          val entityFuture: Future[HttpEntity.Strict] = (personActor ? FindPerson(pin))
+            .mapTo[Option[Person]]
+            .map(_.toJson.prettyPrint)
+            .map(toHttpEntity)
+          complete(entityFuture)
+        } ~ pathEndOrSingleSlash {
+          println(s"retrieve all people")
+          val entityFuture: Future[HttpEntity.Strict] = (personActor ? FindAllPeople)
+            .mapTo[List[Person]]
+            .map(_.toJson.prettyPrint)
+            .map(toHttpEntity)
+          complete(entityFuture)
+        }
+      } ~ (pathPrefix("api" / "people") & post) {
+        extractRequest { (httpRequest: HttpRequest) =>
+          val strictEntityFuture: Future[HttpEntity.Strict] = httpRequest.entity.toStrict(3 seconds)
+
+          val entity = strictEntityFuture.flatMap { strictEntity =>
+            val personJsonString: String = strictEntity.data.utf8String
+            val person: Person = personJsonString.parseJson.convertTo[Person]
+            val personCreatedFuture: Future[PersonCreated] = (personActor ? CreatePerson(person)).mapTo[PersonCreated]
+            personCreatedFuture.map { msg: PersonCreated =>
+              println(s"add that person to your database. httpRequest: $httpRequest")
+              toHttpEntity(person.toJson.prettyPrint)
+            }
+          }
+          complete(entity)
+        }
+      }
+
+    println("http GET localhost:8080/api/people")
+    println("http GET localhost:8080/api/people/1")
+    println("http GET localhost:8080/api/people?pin=1")
+    println("http POST localhost:8080/api/people < src/main/resources/json/person.json")
+    Http()
+      .newServerAt("localhost", 8080)
+      .bindFlow(personRoutes)
   }
 }
-
-case class Person(pin: Int, name: String)
