ee362fe40a23530654998f0438b7548b63ee8ec8
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-23 09:43:38 +0100

    :construction: Akka HTTP high level Guitar post inventory qtd

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/GuitarRestHighLevelApi.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/GuitarRestHighLevelApi.scala
@@ -28,7 +28,7 @@ object GuitarRestHighLevelApi extends GuitarStoreJsonProtocol {
     implicit val system = ActorSystem("GuitarRestHighLevelApi")
     import GuitarDB._
     import system.dispatcher
-    val guitarDbActor = system.actorOf(Props[GuitarDB], "LowLevelGuitarDB")
+    val guitarDbActor = system.actorOf(Props[GuitarDB], "GuitarRestHighLevelApi")
     val guitarList = List(
       Guitar("Fender", "Stratocaster"),
       Guitar("Gibson", "Les Paul"),
@@ -42,11 +42,11 @@ object GuitarRestHighLevelApi extends GuitarStoreJsonProtocol {
 
     val guitarServerRoutes =
       pathPrefix("api" / "guitar") {
-        (path(IntNumber) | parameter('id.as[Int])) { (guitarId: Int) => {
+        (path(IntNumber) | parameter('id.as[Int])) { (guitarId: Int) =>
           get {
             // println(s"I found the guitar $guitarId")
             val guitarsFuture: Future[Option[Guitar]] = (guitarDbActor ? FindGuitar(guitarId)).mapTo[Option[Guitar]]
-            val entityFuture = guitarsFuture.map {
+            val entityFuture: Future[HttpResponse] = guitarsFuture.map {
               case None => HttpResponse(StatusCodes.NotFound)
               case Some(guitar) =>
                 HttpResponse(
@@ -58,10 +58,35 @@ object GuitarRestHighLevelApi extends GuitarStoreJsonProtocol {
             }
             complete(entityFuture)
           }
-        }
+        } ~ (path("inventory") & parameter('inStock.as[Boolean])) { inStock =>
+          get {
+            val guitarsFuture: Future[List[Guitar]] = (guitarDbActor ? FindGuitarsInStock(inStock)).mapTo[List[Guitar]]
+            val entityFuture: Future[HttpResponse] = guitarsFuture.map { guitars =>
+              HttpResponse(
+                entity = HttpEntity(
+                  ContentTypes.`application/json`,
+                  guitars.toJson.prettyPrint
+                )
+              )
+            }
+            complete(entityFuture)
+          }
+        } ~ (path("inventory") & parameter('id.as[Option[Int]]) & parameter('quantity.as[Option[Int]])) { (guitarId, guitarQuantity) =>
+          post {
+            val validGuitarResponseFuture: Option[Future[HttpResponse]] = for {
+              id <- guitarId
+              quantity <- guitarQuantity
+            } yield {
+              // construct the HTTP response
+              val newGuitarFuture: Future[Option[Guitar]] = (guitarDbActor ? AddQuantity(id, quantity)).mapTo[Option[Guitar]]
+              newGuitarFuture.map(_ => HttpResponse(StatusCodes.OK))
+            }
+            val entityFuture: Future[HttpResponse] = validGuitarResponseFuture.getOrElse(Future(HttpResponse(StatusCodes.BadRequest)))
+            complete(entityFuture)
+          }
         } ~ get {
           val guitarsFuture: Future[List[Guitar]] = (guitarDbActor ? FindAllGuitars).mapTo[List[Guitar]]
-          val entityFuture = guitarsFuture.map { guitars =>
+          val entityFuture: Future[HttpResponse] = guitarsFuture.map { guitars =>
             HttpResponse(
               entity = HttpEntity(
                 ContentTypes.`application/json`,
