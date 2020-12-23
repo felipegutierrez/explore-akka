a2d67a4a4d6d2a8c7d8d054ca11c2800050c2d22
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-23 15:22:17 +0100

    :construction: handling rejections alternative

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/MarshallingJSON.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/http/highlevel/MarshallingJSON.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.{parameter, _}
-import akka.http.scaladsl.server.{Rejection, RejectionHandler}
+import akka.http.scaladsl.server.{MissingQueryParamRejection, Rejection, RejectionHandler}
 import akka.pattern.ask
 import akka.util.Timeout
 import spray.json._
@@ -139,6 +139,15 @@ object MarshallingJSON extends PlayerJsonProtocol with SprayJsonSupport {
           }
         }
       }
+
+    // Alternative: defining an implicit rejection handler
+    implicit val customRejectionHandler = RejectionHandler.newBuilder()
+      .handle {
+        case m: MissingQueryParamRejection =>
+          println(s"I got a query param rejection: $m")
+          complete("Rejected query param")
+      }
+      .result()
 
     println("http GET localhost:8080/api/player")
     println("http GET localhost:8080/api/player/class/Warrior")
