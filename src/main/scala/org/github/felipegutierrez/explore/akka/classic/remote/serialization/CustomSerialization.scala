8ee84a70194d6a9a2614ca6f800c1a26e5702cd5
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-29 12:16:59 +0100

    :construction: (de)serializing java objects on Akka-remote

---
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/remote/serialization/CustomSerialization.scala
@@ -0,0 +1,71 @@
+package org.github.felipegutierrez.explore.akka.classic.remote.serialization
+
+import akka.actor.{ActorSystem, Props}
+import akka.serialization.Serializer
+import com.typesafe.config.ConfigFactory
+
+import spray.json._
+
+case class Person(name: String, age: Int)
+
+class PersonSerializer extends Serializer {
+
+  val SEPARATOR = "//"
+
+  override def identifier: Int = 74238
+
+  override def toBinary(o: AnyRef): Array[Byte] = o match {
+    case person @ Person(name, age) =>
+      // [John||32]
+      println(s"Serializing $person")
+      s"[$name$SEPARATOR$age]".getBytes()
+    case _ => throw new IllegalArgumentException("only persons are supported for this serializer")
+  }
+
+  override def fromBinary(bytes: Array[Byte], manifest: Option[Class[_]]): AnyRef = {
+    val string = new String(bytes)
+    val values = string.substring(1, string.length - 1).split(SEPARATOR)
+    val name = values(0)
+    val age = values(1).toInt
+
+    val person = Person(name, age)
+    println(s"Deserialized $person")
+    person
+  }
+
+  override def includeManifest: Boolean = false
+}
+
+object CustomSerialization_Local {
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+  def run() = {
+    val config = ConfigFactory.parseString(
+      """
+        |akka.remote.artery.canonical.port = 2551
+    """.stripMargin)
+      .withFallback(ConfigFactory.load().getConfig("customSerializablePerson"))
+
+    val system = ActorSystem("LocalSystem", config)
+    val actorSelection = system.actorSelection("akka://RemoteSystem@localhost:2552/user/remoteActor")
+
+    actorSelection ! Person("Alice", 23)
+  }
+}
+
+object CustomSerialization_Remote {
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+  def run() = {
+    val config = ConfigFactory.parseString(
+      """
+        |akka.remote.artery.canonical.port = 2552
+    """.stripMargin)
+      .withFallback(ConfigFactory.load().getConfig("customSerializablePerson"))
+
+    val system = ActorSystem("RemoteSystem", config)
+    val simpleActor = system.actorOf(Props[SimpleActor], "remoteActor")
+  }
+}
