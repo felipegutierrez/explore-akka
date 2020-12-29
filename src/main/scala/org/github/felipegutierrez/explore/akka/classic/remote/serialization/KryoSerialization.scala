8666c3131036a6dc6571ad66610c6de07eb1d68c
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-12-29 14:12:09 +0100

    :construction: (de)serializing java objects on Akka-remote with Kryo

---
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/remote/serialization/KryoSerialization.scala
@@ -0,0 +1,42 @@
+package org.github.felipegutierrez.explore.akka.classic.remote.serialization
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+// case class Book(name: String, year: Int)
+
+object KryoSerialization_Local {
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+
+  def run() = {
+    val config = ConfigFactory.parseString(
+      """
+        |akka.remote.artery.canonical.port = 2551
+    """.stripMargin)
+      .withFallback(ConfigFactory.load().getConfig("kryoSerializablePerson"))
+
+    val system = ActorSystem("LocalSystem", config)
+    val actorSelection = system.actorSelection("akka://RemoteSystem@localhost:2552/user/remoteActor")
+
+    actorSelection ! Person("Alice", 23)
+  }
+}
+
+object KryoSerialization_Remote {
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+
+  def run() = {
+    val config = ConfigFactory.parseString(
+      """
+        |akka.remote.artery.canonical.port = 2552
+    """.stripMargin)
+      .withFallback(ConfigFactory.load().getConfig("kryoSerializablePerson"))
+
+    val system = ActorSystem("RemoteSystem", config)
+    val simpleActor = system.actorOf(Props[SimpleActor], "remoteActor")
+  }
+}
