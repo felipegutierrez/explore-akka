1a556d2fc903134e8f5028f25717e88f8f3e1026
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-01-06 10:01:47 +0100

    :wrench: refactoring

--- src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/AkkaStreamWithKinesis.scala
+++ src/main/scala/org/github/felipegutierrez/explore/akka/classic/streams/graphs/AkkaStreamWithKinesis.scala
@@ -6,28 +6,34 @@ import akka.stream.scaladsl.{GraphDSL, Merge, Partition, RunnableGraph, Sink, So
 
 import scala.concurrent.duration._
 
-object AkkaStreamWithKinesis extends App {
-  implicit val system = ActorSystem("AkkaStreamWithKinesisSystem")
+object AkkaStreamWithKinesis {
+  //  def main(args: Array[String]): Unit = {
+  //    run()
+  //  }
 
-  val source = Source(1 to 1000).throttle(5, 1 second)
-  val sink = Sink.foreach[Int](println(_))
+  def run() = {
+    implicit val system = ActorSystem("AkkaStreamWithKinesisSystem")
 
-  val graph = RunnableGraph.fromGraph(
-    GraphDSL.create(source, sink)
-    ((source, sink) => Seq(source, sink)) {
-      implicit builder =>
-        (source, sink) =>
-          import akka.stream.scaladsl.GraphDSL.Implicits._
-          val partition = builder.add(Partition[Int](2, flow => {
-            1
-          }))
-          val merge = builder.add(Merge[Int](2))
+    val source = Source(1 to 1000).throttle(5, 1 second)
+    val sink = Sink.foreach[Int](println(_))
 
-          source ~> partition.in
-          partition.out(0) ~> merge.in(0)
-          partition.out(1) ~> merge.in(1)
-          merge.out ~> sink
+    val graph = RunnableGraph.fromGraph(
+      GraphDSL.create(source, sink)
+      ((source, sink) => Seq(source, sink)) {
+        implicit builder =>
+          (source, sink) =>
+            import akka.stream.scaladsl.GraphDSL.Implicits._
+            val partition = builder.add(Partition[Int](2, flow => {
+              1
+            }))
+            val merge = builder.add(Merge[Int](2))
 
-          ClosedShape
-    }).run()
+            source ~> partition.in
+            partition.out(0) ~> merge.in(0)
+            partition.out(1) ~> merge.in(1)
+            merge.out ~> sink
+
+            ClosedShape
+      }).run()
+  }
 }
